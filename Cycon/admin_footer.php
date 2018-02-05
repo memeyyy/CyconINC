680f946603c6412e46ea051a5814e1b094240714
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2018-02-05 16:22:26 +0800

    admin

---
+++ Cycon/admin_footer.php
@@ -0,0 +1,54 @@
+    <!--Footer-->
+    <footer class="page-footer center-on-small-only mt-0">
+
+        <!--Footer links-->
+        <div class="container">
+            <div class="row">
+                <!--First column-->
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="title mb-3">
+                        <strong>Cycon Inc</strong>
+                    </h5>
+                    <p>Substation company description here</p>
+                </div>
+                <!--/.First column-->
+                <hr class="w-100 clearfix d-sm-none">
+                <!--Second column-->
+                <div class="col-lg-2 col-md-6 ml-auto">
+                    <h5 class="title mb-3">
+                        <strong>link</strong>
+                    </h5>
+                    <ul>
+                    <li>
+                        <a href="#">Project Monitoring</a>
+                    </li>
+                    <li>
+                        <a href="#">Warehouse Inventory</a>
+                    </li>
+                    <li>
+                        <a href="#">Payment Transaction</a>
+                    </li>
+                    <li>
+                        <a href="#">Report</a>
+                    </li>
+
+                    </ul>
+                </div>
+                <!--/.Second column-->
+               
+            </div>
+        </div>
+        <!--/.Footer links-->
+
+        <!--Copyright-->
+        <div class="footer-copyright">
+            <div class="container-fluid">
+                © 2018 Copyright:
+                <a href="https://www.cycon.com"> CYCON INC. </a>
+
+            </div>
+        </div>
+        <!--/.Copyright-->
+
+    </footer>
+    <!--/.Footer-->
