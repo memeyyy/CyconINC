2bc706e177b4943ae40c0b1bb94455a8aaa74dd2
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2018-02-05 15:26:17 +0800

    New Cycon Design

---
+++ Cycon/general_script.php
@@ -0,0 +1,13 @@
+    <!-- SCRIPTS -->
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+
+    <!-- Bootstrap dropdown -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
